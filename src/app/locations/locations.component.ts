a1f219a8c85289b40f2d78be3003b15ad4518f85
danisylvester <maria.sylvester10@gmail.com>
2020-11-17 21:28:24 -0600

    Added a custom scroll-spy to the locations component.

--- src/app/locations/locations.component.ts
+++ src/app/locations/locations.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, HostListener, Inject } from '@angular/core';
 import { DOCUMENT, getLocaleDateFormat } from '@angular/common';
+import isVisible from 'is-element-visible';
 
 
 declare var $: any;
@@ -11,50 +12,100 @@ declare var $: any;
 })
 export class LocationsComponent implements OnInit {
 
+
+  @HostListener('window:scroll', ['$event']) onScroll(e) {
+    let ele = document.getElementById('loc1');
+    let x = this.elementInViewport(ele);
+    ele = document.getElementById('menuItem1');
+    if (x){
+      if (!ele.classList.contains('highlight')){
+        ele.classList.add('highlight');
+      }
+    }else{
+        ele.classList.remove('highlight');
+    }
+
+    let ele2 = document.getElementById('loc2');
+    let x2 = this.elementInViewport(ele2);
+    ele2 = document.getElementById('menuItem2');
+    if (x2){
+      if (!ele2.classList.contains('highlight')){
+        ele2.classList.add('highlight');
+      }
+    }else{
+        ele2.classList.remove('highlight');
+    }
+
+    let ele3 = document.getElementById('loc3');
+    let x3 = this.elementInViewport(ele3);
+    ele3 = document.getElementById('menuItem3');
+    if (x3){
+      if (!ele3.classList.contains('highlight')){
+        ele3.classList.add('highlight');
+      }
+    }else{
+        ele3.classList.remove('highlight');
+    }
+
+    let ele4 = document.getElementById('loc4');
+    let x4 = this.elementInViewport(ele4);
+    ele4 = document.getElementById('menuItem4');
+    if (x4){
+      if (!ele4.classList.contains('highlight')){
+        ele4.classList.add('highlight');
+      }
+    }else{
+        ele4.classList.remove('highlight');
+    }
+
+    let ele5 = document.getElementById('loc5');
+    let x5 = this.elementInViewport(ele5);
+    ele5 = document.getElementById('menuItem5');
+    if (x5){
+      if (!ele5.classList.contains('highlight')){
+        ele5.classList.add('highlight');
+      }
+    }else{
+        ele5.classList.remove('highlight');
+    }
+
+    let ele6 = document.getElementById('loc6');
+    let x6 = this.elementInViewport(ele6);
+    ele6 = document.getElementById('menuItem6');
+    if (x6){
+      if (!ele6.classList.contains('highlight')){
+        ele6.classList.add('highlight');
+      }
+    }else{
+        ele6.classList.remove('highlight');
+    }
+  }
+
   public highlight: boolean = false;
 
-  public loc1(inViewPort: boolean): void{
-    if (inViewPort){
-      console.log('hi loc1');
-    }
-    else{
-      console.log('bye loc1');
-    }
-  }
-
-  public loc2(inViewPort: boolean): void{
-    if (inViewPort){
-      console.log('hi loc2');
-    }
-    else{
-      console.log('bye loc2');
-    }
-  }
-
-  public loc3(inViewPort: boolean): void{
-   
-  }
-
-  public loc4(inViewPort: boolean): void{
-   
-  }
-
-  public loc5(inViewPort: boolean): void{
-   
-  }
-
-  public loc6(inViewPort: boolean): void{
-
-  }
-
-
-
   constructor() {
-    
+
   }
 
   ngOnInit(): void {
 
   }
+  private elementInViewport(el) {
+   
+    // Special bonus for those using jQuery
+    if (typeof $ === 'function' && el instanceof $) {
+      el = el[0];
+  }
+
+  var rect = el.getBoundingClientRect();
+
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+  );
+  }
 }
+
 
