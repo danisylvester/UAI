7bcf24dbc9823bc39536638bade7168bfc163077
danisylvester <maria.sylvester10@gmail.com>
2020-11-19 14:52:26 -0600

    fixed dependencies and server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/uaigold'));
+app.use(express.static(__dirname + '/dist/gold'));
 
 app.get('/*',function(req,res){
-res.sendFile(path.join(__dirname+'/dist/uaigold/index.html'));
+res.sendFile(path.join(__dirname+'/dist/gold/index.html'));
 })
 app.listen(process.env.PORT || 8080);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingComponent } from './landing/landing.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { HomeServicesComponent } from './home-services/home-services.component';
 import { LocationsComponent } from './locations/locations.component';
 import { HomeContactComponent } from './home-contact/home-contact.component';
@@ -14,7 +14,6 @@ import { ScrollSpyDirective } from './scroll-spy.directive';
 import { FooterComponent } from './footer/footer.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HomeComponent } from './home/home.component';
-import { InViewportModule } from '@thisissoon/angular-inviewport';
 import { ContactComponent } from './contact/contact.component';
 import { RouterModule } from '@angular/router';
 
@@ -38,10 +37,9 @@ import { RouterModule } from '@angular/router';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
-    InViewportModule,
     RouterModule
   ],
-  providers: [ ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
